e954ec59404bb0b14c76ed564628f03c5f689b30
nemo <luoxincheng@didiglobal.com>
2021-04-26 13:41:56 +0800

    save

--- src/router/demo.js
+++ src/router/demo.js
@@ -10,4 +10,47 @@ router.get("/demo", async (ctx, next) => {
     ctx.body = 'demo'
 })
 
+router.all("/upload", async (ctx, next) => {
+    ctx.body = await new Promise((resolve) => {
+        ctx.req.pipe(
+        request({
+            url: "xxx",
+            headers: ctx.header,
+            method: "post",
+        }, (err, res) => {
+            let resBody = JSON.parse(res.body);
+            resolve({
+                code: resBody.errno,
+                message: resBody.errmsg,
+                data: resBody.data
+            });
+        })
+        );
+  });
+})
+
+router.all("/download", async (ctx, next) => {
+    ctx.body = request({
+        url: "xxx",
+        headers: ctx.header,
+        method: "post",
+        json: true,
+        body: ctx.request.body,
+    });
+})
+
+router.all("/form-upload", async (ctx, next) => {
+    ctx.body = await new Promise((resolve) => {
+        request.post(
+            {
+                url: "xxx",
+                form: ctx.request.body,
+            },
+            function (e, res, body) {
+                resolve(JSON.parse(body).data);
+            }
+        );
+    });
+})
+
 export default router;
